981688222c06a2d23a0449945f42c86eb4803a94
Jeongsik Mun <farfs.dev@gmail.com>
2019-08-11 14:38:14 +0900

    init

---
+++ app/src/main/java/com/codechacha/myapplication/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.codechacha.myapplication
+
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import android.app.AppOpsManager.OPSTR_GET_USAGE_STATS
+import android.app.AppOpsManager
+import android.app.AppOpsManager.MODE_ALLOWED
+import android.os.Process
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            if (!checkForPermission()) {
+                Log.i(TAG, "The user may not allow the access to apps usage. ")
+                Toast.makeText(
+                    this,
+                    "Failed to retrieve app usage statistics. " +
+                            "You may need to enable access for this app through " +
+                            "Settings > Security > Apps with usage access",
+                    Toast.LENGTH_LONG
+                ).show()
+                startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            } else {
+                val usageStats = getAppUsageStats()
+                showAppUsageStats(usageStats)
+            }
+        }
+    }
+
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
+        return mode == MODE_ALLOWED
+    }
+
+    private fun showAppUsageStats(usageStats: MutableList<UsageStats>) {
+        usageStats.sortWith(Comparator { right, left ->
+            compareValues(left.lastTimeUsed, right.lastTimeUsed)
+        })
+
+        usageStats.forEach { it ->
+            Log.d(TAG, "packageName: ${it.packageName}, lastTimeUsed: ${Date(it.lastTimeUsed)}, " +
+                    "totalTimeInForeground: ${it.totalTimeInForeground}")
+        }
+    }
+
+    private fun getAppUsageStats(): MutableList<UsageStats> {
+        val cal = Calendar.getInstance()
+        cal.add(Calendar.YEAR, -1)
+
+        val usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val queryUsageStats = usageStatsManager
+            .queryUsageStats(
+                UsageStatsManager.INTERVAL_DAILY, cal.timeInMillis,
+                System.currentTimeMillis()
+            )
+        return queryUsageStats
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
